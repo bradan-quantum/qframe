6fb0a27cb0faf7f7d75f957191c17b4be32ff97d
Fintan Bolton <fintan.bolton@iceye.fi>
2026-01-30 13:43:48 +0100

    Added the ch() gate code, including test

---
+++ src/qframe/alg_primitives/ch.py
@@ -0,0 +1,60 @@
+# ********************************************************************************
+# * Copyright (c) 2026 Bradan Quantum GmbH
+# *
+# * This program and the accompanying materials are made available under the
+# * terms of the Eclipse Public License 2.0 which is available at
+# * http://www.eclipse.org/legal/epl-2.0.
+# *
+# * This Source Code may also be made available under the following Secondary
+# * Licenses when the conditions for such availability set forth in the Eclipse
+# * Public License, v. 2.0 are satisfied: GNU General Public License, version 2
+# * with the GNU Classpath Exception which is
+# * available at https://www.gnu.org/software/classpath/license.html.
+# *
+# * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+# ********************************************************************************
+
+import qrisp
+from qrisp import h
+
+
+@qrisp.gate_wrap(name='Ch')
+def choose_gate(a, b, c):
+    qrisp.cx(c, b)
+    for k in range(len(c)):
+        qrisp.mcx([a[k], b[k]], c[k])
+
+def _recip_choose_single_bit_gate(a:qrisp.Qubit, b:qrisp.Qubit, c:qrisp.Qubit, phase_oracle:qrisp.Qubit):
+    """
+    Applies the single-qubit variant of the reciprocal choose() gate
+    """
+    qrisp.cx(b, c)
+    with qrisp.control(c):
+        qrisp.mcx([a, b], phase_oracle)
+        h(a)
+        h(b)
+        qrisp.mcx([a, b], phase_oracle)
+        qrisp.swap(a, b)
+
+
+@qrisp.gate_wrap(name='rCh')
+def recip_choose_gate(a, b, c, phase_oracle:qrisp.Qubit):
+    """
+    Applies the multi-qubit (and single-bit) variant of the reciprocal choose() gate
+    """
+    if isinstance(c, qrisp.Qubit):
+        _recip_choose_single_bit_gate(a, b, c, phase_oracle)
+    elif isinstance(c, qrisp.QuantumVariable):
+        for k in range(len(c)):
+            _recip_choose_single_bit_gate(a[k], b[k], c[k], phase_oracle)
+    else:
+        raise Exception('Arguments a, b, c should either be all Qubit or all QuantumVariable types')
+
+@qrisp.gate_wrap(name='rCh_1')
+def recip_choose_first_order_gate(a:qrisp.Qubit, b:qrisp.Qubit, c:qrisp.Qubit):
+    """
+    Applies the circuit for the **first order** reciprocal choose() gate
+
+    An optimization of the reciprocal choose gate, got by dropping the second half of the circuit. This optimization should only be enabled in the appropriate context: in some cases, after applying this gate, only the 'c' qubit (which references the output of the Ch() function) is used before uncomputing (by applying the inverse reciprocal choose gate). In this case, the second half of the reciprocal choose circuit is superfluous, because it change qubits 'a' and 'b' only and this change gets immediately reversed by the uncompute.
+    """
+    qrisp.cx(b, c)
---
+++ tests/alg_primitives/test_ch.py
@@ -0,0 +1,132 @@
+# ********************************************************************************
+# * Copyright (c) 2026 Bradan Quantum GmbH
+# *
+# * This program and the accompanying materials are made available under the
+# * terms of the Eclipse Public License 2.0 which is available at
+# * http://www.eclipse.org/legal/epl-2.0.
+# *
+# * This Source Code may also be made available under the following Secondary
+# * Licenses when the conditions for such availability set forth in the Eclipse
+# * Public License, v. 2.0 are satisfied: GNU General Public License, version 2
+# * with the GNU Classpath Exception which is
+# * available at https://www.gnu.org/software/classpath/license.html.
+# *
+# * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+# ********************************************************************************
+
+import pytest
+
+import qrisp
+from qrisp import h
+
+from qframe.alg_primitives.ch import choose_gate, recip_choose_gate, recip_choose_first_order_gate
+
+
+@pytest.fixture
+def n_float():
+    return 2
+
+@pytest.fixture
+def target():
+    return 3
+
+
+def phase_oracle_init(p):
+    qrisp.z(qrisp.h(p))
+
+def oracle_function(a:qrisp.QuantumFloat, b:qrisp.QuantumFloat, c:qrisp.QuantumFloat, target):
+    choose_gate(a, b, c)
+    for i in range(target.bit_length()):
+        if (target & 1<<i): qrisp.x(c[i])
+
+def recip_oracle_function(a:qrisp.QuantumFloat, b:qrisp.QuantumFloat, c:qrisp.QuantumFloat, anc:qrisp.Qubit):
+    phase_oracle_init(anc)
+    recip_choose_gate(a, b, c, anc)
+
+def recip_oracle_function_optimized(a:qrisp.QuantumFloat, b:qrisp.QuantumFloat, c:qrisp.QuantumFloat):
+    recip_choose_first_order_gate(a, b, c)
+
+
+class Test_Ch:
+    def test_ch_single_bit(self):
+        # Initialize variables
+        a = qrisp.QuantumFloat(1, name='a')
+        b = qrisp.QuantumFloat(1, name='b')
+        c = qrisp.QuantumFloat(1, name='c')
+        anc = qrisp.QuantumVariable(1, name='anc')
+
+        # Prepare the quantum state in an equal-weighted superposition (Walsh-Hadamard transform)
+        h(a)
+        h(b)
+        h(c)
+
+        qrisp.barrier(a[0] + b[0] + c[0])
+
+        # Single partial oracle iteration
+        with qrisp.conjugate(oracle_function)(a, b, c, 0):
+            qrisp.barrier(a[0] + b[0] + c[0])
+            qrisp.s(c)
+            qrisp.barrier(a[0] + b[0] + c[0])
+        qrisp.barrier(a[0] + b[0] + c[0])
+        h(a)
+        h(b)
+        h(c)
+        qrisp.barrier(a[0] + b[0] + c[0])
+        # with qrisp.conjugate(recip_oracle_function)(a, b, c, anc[0]):
+        with qrisp.conjugate(recip_oracle_function_optimized)(a, b, c):
+            qrisp.barrier(a[0] + b[0] + c[0])
+            qrisp.s(c)
+            qrisp.barrier(a[0] + b[0] + c[0])
+        qrisp.barrier(list(a) + list(b) + list(c))
+        h(c)
+        h(b)
+        h(a)
+
+        # Show the circuit
+        print(c.qs)
+        # Show result
+        print(qrisp.multi_measurement([a, b, c]))
+
+    def test_ch_multi_bit(self, n_float, target):
+        # Initialize variables
+        a = qrisp.QuantumFloat(n_float, name='a')
+        b = qrisp.QuantumFloat(n_float, name='b')
+        c = qrisp.QuantumFloat(n_float, name='c')
+        anc = qrisp.QuantumVariable(1, name='anc')
+
+        # Prepare the quantum state in an equal-weighted superposition (Walsh-Hadamard transform)
+        h(a)
+        h(b)
+        h(c)
+
+        qrisp.barrier(list(a) + list(b) + list(c))
+
+        # Single partial oracle iteration
+        with qrisp.conjugate(oracle_function)(a, b, c, target):
+            qrisp.barrier(list(a) + list(b) + list(c))
+            qrisp.s(c)
+            qrisp.barrier(list(a) + list(b) + list(c))
+        qrisp.barrier(list(a) + list(b) + list(c))
+        h(a)
+        h(b)
+        h(c)
+        qrisp.barrier(list(a) + list(b) + list(c))
+        # with qrisp.conjugate(recip_oracle_function)(a, b, c, anc[0]):
+        with qrisp.conjugate(recip_oracle_function_optimized)(a, b, c):
+            qrisp.barrier(list(a) + list(b) + list(c))
+            qrisp.s(c)
+            qrisp.barrier(list(a) + list(b) + list(c))
+        qrisp.barrier(list(a) + list(b) + list(c))
+        h(c)
+        h(b)
+        h(a)
+
+        # Show the circuit
+        print(c.qs)
+        # Show result
+        print(qrisp.multi_measurement([a, b, c]))
+
+    def test_ch_full_reciprocal_gate(self):
+        pass
+        # ToDo: The existing tests do NOT really test the full reciprocal gate, because they only
+        # depend on the first order effects of the gate, not the full gate circuit.
